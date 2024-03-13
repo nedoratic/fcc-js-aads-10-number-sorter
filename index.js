9f4b5c983cae97e0e566f52c084fa55ad55eea8a
nedoratic <nedoratic@gmail.com>
2024-03-13 22:37:13 +0100

    sort input array function added

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // DOM element variable
 const sortButton = document.getElementById('sort');
+
+// Sort Input Array
+const sortInputArray = (event) => {
+	event.preventDefault();
+
+	const inputValues = [...document.getElementsByClassName('values-dropdown')].map((dropdown) => Number(dropdown.value));
+
+	const sortedValues = bubbleSort(inputValues);
+
+	updateUI(sortedValues);
+};
