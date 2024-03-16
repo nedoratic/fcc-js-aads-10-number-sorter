2838530ee1e44c82f31d4f1c881c1be2457c5d1e
nedoratic <nedoratic@gmail.com>
2024-03-16 09:26:05 +0100

    insertion sort algorithm added

--- index.js
+++ index.js
@@ -54,5 +54,20 @@ const selectionSort = (array) => {
 	return array;
 };
 
+// Insertion Sort Algorithm
+const insertionSort = (array) => {
+	for (let i = 1; i < array.length; i++) {
+		const currValue = array[i];
+		let j = i - 1;
+
+		while (j >= 0 && array[j] > currValue) {
+			array[j + 1] = array[j];
+			j--;
+		}
+		array[j + 1] = currValue;
+	}
+	return array;
+};
+
 // Click Event Listener
 sortButton.addEventListener('click', sortInputArray);
