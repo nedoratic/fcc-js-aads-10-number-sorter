59c48df9991f9e1ad5a126cb9fb350a2a3affefb
nedoratic <nedoratic@gmail.com>
2024-03-16 09:24:15 +0100

    bubble sort algorithm added

--- index.js
+++ index.js
@@ -20,5 +20,20 @@ const updateUI = (array = []) => {
 	});
 };
 
+// Bubble Sort
+const bubbleSort = (array) => {
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array.length - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				const temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+
+	return array;
+};
+
 // Click Event Listener
 sortButton.addEventListener('click', sortInputArray);
